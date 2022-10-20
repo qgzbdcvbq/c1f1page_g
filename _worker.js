21a6269951ce62a3b7a44ff2a7b3300061d1adf4
archvm <archvm>
2022-10-20 11:13:47 +0800

    to stagepage

--- _worker.js
+++ _worker.js
@@ -2,7 +2,8 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'google.com'
+      //url.hostname = 'google.com'
+      url.hostname = 'www.statpage.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
